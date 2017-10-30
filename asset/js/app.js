ad22e55f8114d288b7dc64bb687ab492362d4e86
johnsyin97 <johnsyin@yahoo.com>
2017-10-30 18:20:05 +0800

    Replies

--- asset/js/app.js
+++ asset/js/app.js
@@ -32,9 +32,25 @@ function convertInterval(interval){
 
 function playSoundWithInterval(notes,interval){
 	var index = 0;
+    var rand = Math.random();
 	let timerId = setTimeout(function tick() {
 		console.log(NOTEPITCH[notes[index]]);
-		playSound(NOTEPITCH[notes[index]],0);
+		if (rand < 0.33){
+            playSound(NOTEPITCH[(notes[index]+1)],0);
+            if (notes[index] == 10){
+                playSound(84,0);
+            }
+		}
+		else if (rand < 0.66){
+            playSound(NOTEPITCH[(notes[index]-1)],0);
+            if (notes[index] == 0){
+                playSound(55,0);
+            }
+		}
+		else{
+            playSound(((NOTEPITCH[(notes[index])])+12),0);
+		}
+
 		if(typeof interval[index] != 'undefined')
 		{
 			index++;
@@ -61,7 +77,7 @@ function playSound(Note,delaytime,Velocity = 127,Volume = 127){
 			MIDI.noteOff(0, note, delay + 0.25);
 		}
 	});
-} 
+}
 
 function preload(){
 	game.load.audio('slowblue', ['asset/audio/Jazz.mp3']);
@@ -122,11 +138,11 @@ function create(){
 	Qkey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
 	Qkey.onDown.add(QKeyEvent, this);
 	Qkey.onUp.add(QKeyEventUp, this);
-	
+
 	Wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 	Wkey.onDown.add(WKeyEvent, this);
 	Wkey.onUp.add(WKeyEventUp, this);
-	
+
 	Ekey = game.input.keyboard.addKey(Phaser.Keyboard.E);
 	Ekey.onDown.add(EKeyEvent, this);
 	Ekey.onUp.add(EKeyEventUp, this);
@@ -163,14 +179,14 @@ function create(){
 	OpenBracketkey.onDown.add(OpenBracketKeyEvent, this);
 	OpenBracketkey.onUp.add(OpenBracketKeyEventUp, this);
 
-	Spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);	
+	Spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	Spacekey.onDown.add(SpaceKeyEvent, this);
 	Spacekey.onUp.add(SpaceKeyEventUp, this);
 
 	/////////////////////////////////////////////
 	// Graphic
 	/////////////////////////////////////////////
-	
+
 
 	// for row 1 and 2
 	// the varaible is keep resuing hehe, since it is lazy to change
@@ -186,10 +202,10 @@ function create(){
     // draw a rectangle
     graphics.lineStyle(0, 0x0000FF, 0);
     for (var i = 0; i < 4; i++) {
-    	graphics.drawRect((noteWidth + noteIntervalX) * i,0,noteWidth,noteHeight);	
+    	graphics.drawRect((noteWidth + noteIntervalX) * i,0,noteWidth,noteHeight);
     }
     for (var i = 0; i < 4; i++) {
-    	graphics.drawRect((noteWidth + noteIntervalX) * i,noteHeight + noteIntervalY,noteWidth,noteHeight);	
+    	graphics.drawRect((noteWidth + noteIntervalX) * i,noteHeight + noteIntervalY,noteWidth,noteHeight);
     }
 
     // For row 3, i am damn lazy to change the variable name lol
@@ -199,23 +215,23 @@ function create(){
 	var noteIntervalY = 50;
 
     for (var i = 0; i < 3; i++) {
-    	graphics.drawRect((noteWidth + noteIntervalX) * i,(noteHeight + noteIntervalY) * 2,noteWidth,noteHeight);	
+    	graphics.drawRect((noteWidth + noteIntervalX) * i,(noteHeight + noteIntervalY) * 2,noteWidth,noteHeight);
     }
 
     // row 4
-    graphics.drawRect(0,(noteHeight + noteIntervalY) * 3,500,noteHeight);	
-    
+    graphics.drawRect(0,(noteHeight + noteIntervalY) * 3,500,noteHeight);
+
 
     window.graphics = graphics;
 }
 
 function update(){
-	
+
 }
 
 function drawRectPadding(x,y,width,height,padding,color){
 	graphics.beginFill(color);
-	graphics.drawRect( x - padding / 2,y - padding / 2,width + padding,height + padding);	
+	graphics.drawRect( x - padding / 2,y - padding / 2,width + padding,height + padding);
 }
 
 
@@ -230,9 +246,9 @@ function QKeyEvent(){
 	graphics.drawRect((80 + 60) * 0,0,80,80);
 
 	drawRectPadding((80 + 60) * 0,0,80,80,-6,pressColor);
-	
-	if(recording){ 
-		console.log(currTime());	
+
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(0);
 	}
@@ -246,8 +262,8 @@ function WKeyEvent(){
 
 	drawRectPadding((80 + 60) * 1,0,80,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(1);
 	}
@@ -261,8 +277,8 @@ function EKeyEvent(){
 
 	drawRectPadding((80 + 60) * 2,0,80,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(2);
 	}
@@ -276,8 +292,8 @@ function RKeyEvent(){
 
 	drawRectPadding((80 + 60) * 3,0,80,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(3);
 	}
@@ -287,12 +303,12 @@ function TKeyEvent(){
 	console.log("T");
 	playSound(67,0);
 	graphics.beginFill(0x000000);
-	graphics.drawRect((80 + 60) * 0,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 0,80 + 50,80,80);
 
 	drawRectPadding((80 + 60) * 0,80 + 50,80,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(4);
 	}
@@ -302,12 +318,12 @@ function YKeyEvent(){
 	console.log("Y");
 	playSound(69,0);
 	graphics.beginFill(0x000000);
-	graphics.drawRect((80 + 60) * 1,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 1,80 + 50,80,80);
 
 	drawRectPadding((80 + 60) * 1,80 + 50,80,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(5);
 	}
@@ -317,12 +333,12 @@ function UKeyEvent(){
 	console.log("U");
 	playSound(72,0);
 	graphics.beginFill(0x000000);
-	graphics.drawRect((80 + 60) * 2,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 2,80 + 50,80,80);
 
 	drawRectPadding((80 + 60) * 2,80 + 50,80,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(6);
 	}
@@ -332,12 +348,12 @@ function IKeyEvent(){
 	console.log("I");
 	playSound(74,0);
 	graphics.beginFill(0x000000);
-	graphics.drawRect((80 + 60) * 3,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 3,80 + 50,80,80);
 
 	drawRectPadding((80 + 60) * 3,80 + 50,80,80,-6,pressColor);
-	
-	if(recording){ 
-		console.log(currTime());	
+
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(7);
 	}
@@ -352,8 +368,8 @@ function OKeyEvent(){
 
 	drawRectPadding((140 + 40) * 0,(80 + 50) * 2,140,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(8);
 	}
@@ -367,8 +383,8 @@ function PKeyEvent(){
 
 	drawRectPadding((140 + 40) * 1,(80 + 50) * 2,140,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(9);
 	}
@@ -382,17 +398,17 @@ function OpenBracketKeyEvent(){
 
 	drawRectPadding((140 + 40) * 2,(80 + 50) * 2,140,80,-6,pressColor);
 
-	if(recording){ 
-		console.log(currTime());	
+	if(recording){
+		console.log(currTime());
 		interval.push(currTime());
 		notes.push(10);
 	}
-}	
+}
 
 function SpaceKeyEvent(){
 	// Press key effect
 	graphics.beginFill(0x000000);
-	graphics.drawRect(0,(80 + 50) * 3,500,80);	
+	graphics.drawRect(0,(80 + 50) * 3,500,80);
 
 	drawRectPadding(0,(80 + 50) * 3,500,80,-6,pressColor);
 
@@ -412,7 +428,7 @@ function SpaceKeyEvent(){
 		interval.push(currTime());
 		console.log("recording start");
 	}
-	
+
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -427,55 +443,55 @@ function QKeyEventUp(){
 
 function WKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 1,0,80,80);	
+	graphics.drawRect((80 + 60) * 1,0,80,80);
 }
 
 function EKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 2,0,80,80);	
+	graphics.drawRect((80 + 60) * 2,0,80,80);
 }
 
 function RKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 3,0,80,80);	
+	graphics.drawRect((80 + 60) * 3,0,80,80);
 }
 
 function TKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 0,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 0,80 + 50,80,80);
 }
 
 function YKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 1,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 1,80 + 50,80,80);
 }
 
 function UKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 2,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 2,80 + 50,80,80);
 }
 
 function IKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((80 + 60) * 3,80 + 50,80,80);	
+	graphics.drawRect((80 + 60) * 3,80 + 50,80,80);
 }
 
 function OKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((140 + 40) * 0,(80 + 50) * 2,140,80);	
+	graphics.drawRect((140 + 40) * 0,(80 + 50) * 2,140,80);
 }
 
 function PKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((140 + 40) * 1,(80 + 50) * 2,140,80);	
+	graphics.drawRect((140 + 40) * 1,(80 + 50) * 2,140,80);
 }
 
 function OpenBracketKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect((140 + 40) * 2,(80 + 50) * 2,140,80);	
+	graphics.drawRect((140 + 40) * 2,(80 + 50) * 2,140,80);
 }
 
 function SpaceKeyEventUp(){
 	graphics.beginFill(normalColor);
-	graphics.drawRect(0,(80 + 50) * 3,500,80);	
+	graphics.drawRect(0,(80 + 50) * 3,500,80);
 }
